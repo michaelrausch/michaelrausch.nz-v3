683eb460f1d178d296ab02e203cbff9d1812e65d
Michael Rausch <michael@rausch.nz>
2020-02-20 23:10:50 +1300

    Restored example environment files

---
+++ src/environments/environment.prod.example.ts
@@ -0,0 +1,18 @@
+export const environment = {
+    production: true,
+    api_url: "",
+    google_maps_api_url: "",
+    employer_content_url: "",
+    cv_download: "",
+    social: {
+        github: "",
+        linkedin: "",
+        facebook: "",
+        px500: "",
+        twitter: ""
+    },
+    version: {
+        app: "",
+        api: ""
+    }
+}; 
